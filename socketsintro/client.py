f3c2c18d0beeb7a7cb4504f76e0c4321e08f5014
Liberbell <liberbell@gmail.com>
2020-11-20 08:56:53 +0900

    client3

--- socketsintro/client.py
+++ socketsintro/client.py
@@ -1,8 +1,9 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 4571))
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("localhost", 4571))
 
-msg = s.recv(1024)
-print("Message from server: ", msg.decode("utf-8"))
-s.close()
+    msg = s.recv(1024)
+    print("Message from server: ", msg.decode("utf-8"))
+    s.close()
