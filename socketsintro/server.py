efc63b400d327c9a92fd4835988a57b258c65b7c
Liberbell <liberbell@gmail.com>
2020-11-20 08:56:01 +0900

    server4

--- socketsintro/server.py
+++ socketsintro/server.py
@@ -1,15 +1,14 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('127.0.0.1',4571))
+    s.listen(5)
 
-s.bind(('127.0.0.1',4571))
-s.listen(5)
+    print("Server is up. Listening for connections....")
 
-print("Server is up. Listening for connections....")
+    client, address = s.accept()
+    print("Connection to ", address, "established.\n")
+    print("Client object: ", client, "\n")
 
-client, address = s.accept()
-print("Connection to ", address, "established.\n")
-print("Client object: ", client, "\n")
-
-client.send(bytes("Hello! Welcome to socket programming.", "utf-8"))
-s.close()
+    client.send(bytes("Hello! Welcome to socket programming.", "utf-8"))
