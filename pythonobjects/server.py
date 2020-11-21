76398ef03eb45a2f623c52d0957849d0f20dc951
Liberbell <liberbell@gmail.com>
2020-11-21 10:17:54 +0900

    server4

--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -1,2 +1,7 @@
 import socket
 from product import Product
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("localhost", 4571))
+
+    python_dictionary = {"a": 1, "b": 2}
