91f6c50518e3c4c9b97e45e74d58a3e2985d311f
Liberbell <liberbell@gmail.com>
2020-11-22 09:18:13 +0900

    server5

--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -16,5 +16,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Connection to ", address, "established\n")
     print("Client object ", client, "\n")
 
-    client.send(python_dictionary)
+    client.send(pickled_dictionary)
     client.send(custom_product)
