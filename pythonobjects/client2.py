1deddaed2fe50a4455dd2e3fffa6b0ce68a62e8c
Liberbell <liberbell@gmail.com>
2020-11-22 15:33:51 +0900

    client1

--- pythonobjects/client2.py
+++ pythonobjects/client2.py
@@ -3,11 +3,9 @@ from product import Product
 import pickle
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	
 	s.connect(("localhost", 4571))
 
 	while True:
-		
 		msg = s.recv(1024)
 
 		if not msg:
