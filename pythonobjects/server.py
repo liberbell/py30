7193adadb1fe59ae68f63133abed10077d9ec0f1
Liberbell <liberbell@gmail.com>
2020-11-22 09:18:01 +0900

    server4

--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -1,10 +1,12 @@
 import socket
 from product import Product
+import pickle
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("localhost", 4571))
 
     python_dictionary = {"a": 1, "b": 2}
+    pickled_dictionary = pickle.dumps(python_dictionary)
     custom_product = Product("P024", "Torch", 13)
 
     s.listen(5)
